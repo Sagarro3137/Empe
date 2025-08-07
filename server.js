6e27cc46c6704bcdbbf31be70960a86b2b21de7f
Sagarro3137 <shuboroy2004@gmail.com>
2025-08-08 04:43:47 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', socket => {
+  socket.on('offer', data => socket.broadcast.emit('offer', data));
+  socket.on('answer', data => socket.broadcast.emit('answer', data));
+  socket.on('candidate', data => socket.broadcast.emit('candidate', data));
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
